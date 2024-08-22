0809dca9219d2c363cf6d8d2bf872832cc7433b3
Phil <philrsheridan@gmail.com>
2024-08-22 15:53:32 -0400

    building models, currently user, calendar, user_calendar, note, reminder: should satisfy relationship requirements

--- server/config.py
+++ server/config.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
@@ -23,6 +24,8 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
 # Instantiate REST API
 api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,73 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    #relationships
+    #many users have many calendars
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes cannot be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        passwrd_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}'
+
+
+class Calendar(db.Model, SerializerMixin):
+    __tablename__ = 'calendars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+
+    #relationships
+    #many users have many calendars
+    #one calendar has many notes
+
+
+class User_Calendar(db.Model, SerializerMixin):
+    __tablename__ = 'user_calendars'
+
+    pass
+    #relationships
+
+
+class Note(db.Model, SerializerMixin):
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    date = db.Column(db.Date)
+
+    #relationships
+    #one calendar has many notes
+
+
+class Reminder(db.Model, SerializerMixin):
+    __tablename__ = 'reminders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    repeating = db.Column(db.String)
+
+    #relationships
+    #one calendar has many reminders
